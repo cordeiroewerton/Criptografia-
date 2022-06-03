7e997d6acfa9f1c1d8a6f16449d4b58d964c7462
cordeiroewerton <67035660+cordeiroewerton@users.noreply.github.com>
2022-06-03 10:59:35 -0300

    Update main.py

--- Monoalfabetica/Vigenere/main.py
+++ Monoalfabetica/Vigenere/main.py
@@ -1,37 +1,39 @@
 import string
-import random
+from random import randint
 
-def mod26(x):
-    y = int(x/26)
-    j = y*26
-    return x-j
+letters = string.ascii_letters + string.punctuation
 
+texto = "Hello"
+chave = [randint(1, 25) for i in range(0, len(texto))]
 
-def cesar(txt, key, carac):
+print("Texto Claro: ", texto)
+
+# Encripta
+def vigenere(txt, key):
     convertido = ""
-    # for para verificar toda o texto
-    for i in txt:
-        if i in carac:
-            num = carac.find(i)
-            num += key
-        # ifs para caso a chave seja menor que 0 e maior que a cadeira de caracteres presentes
-        if num >= len(carac):
-            num = num - len(carac)
-        elif num < 0:
-            num = num + len(carac)
-        convertido += carac[num]
+
+    for j, i in enumerate(txt, start=0):
+        if i in letters:
+            num = letters.find(i)
+            num += key[j]
+            convertido += letters[num]
     return convertido
 
 
-#definindo os caracteres e textos
+print("Encriptação por Vigenère: ", vigenere(texto, chave))
 
-x=int(input("AAA:"))
-y=mod26(x)
-print(y)
+# Decripta
+def D_vigenere(txt, key):
+    convertido = ""
 
-carac = string.ascii_letters + string.punctuation
-texto = input("DIGITE O TEXTO CLARO:")
-chave = [random.randint(0,25) for i in range(0,len(texto))]
-convertido = cesar(texto,chave,carac)
-print(convertido)
+    for j, i in enumerate(txt, start=0):
+        if i in letters:
+            num = letters.find(i)
+            num -= key[j]
+            convertido += letters[num]
+    return convertido
+
+
+print("Vigenère Decriptada: ", D_vigenere(vigenere(texto, chave), chave))
+print("Chave: ", chave)
 
